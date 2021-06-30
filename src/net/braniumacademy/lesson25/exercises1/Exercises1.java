65b244660defaa29818178fc5315876d40a62385
Trieu Than <trieuthan92pro@gmail.com>
2021-06-30 19:59:12 +0700

    Add exercises 1 l2.5

--- src/net/braniumacademy/lesson25/exercises1/Exercises1.java
+++ src/net/braniumacademy/lesson25/exercises1/Exercises1.java
@@ -2,7 +2,7 @@ package net.braniumacademy.lesson25.exercises1;
 
 public class Exercises1 {
     public static void main(String[] args) {
-        DoublyLinkedList<String> stringList = new DoublyLinkedList<>();
+        LinkedList<String> stringList = new LinkedList<>();
         stringList.insertTail("Linh");
         stringList.insertTail("Nam");
         stringList.insertTail("Ngân");
@@ -12,18 +12,18 @@ public class Exercises1 {
         stringList.insertTail("Nhung");
         // hiển thị danh sách phần tử
         System.out.println("Các phần tử trong danh sách: ");
-        stringList.traversalFromHead();
+        stringList.showList();
         removeNode(stringList, "Nam");
         removeNode(stringList, "Hoàng");
         removeNode(stringList, "Nhung");
     }
 
-    private static void removeNode(DoublyLinkedList<String> stringList, String key) {
+    private static void removeNode(LinkedList<String> stringList, String key) {
         System.out.println("Dữ liệu trong node cần xóa: " + key);
         var result = stringList.removeNode(key);
         if (result) { // xóa thành công
             System.out.println("Xóa thành công! Danh sách sau khi xóa: ");
-            stringList.traversalFromHead();
+            stringList.showList();
         } else {
             System.out.println("Xóa thất bại. Node cần xóa không tồn tại.");
         }
