f4596666c5cc85f7f99b7b39c77eca7501e4a1f7
Trieu Than <trieuthan92pro@gmail.com>
2021-07-05 19:36:02 +0700

    Add ex1 l2.9

---
+++ src/net/braniumacademy/lesson28/exercises1/SortingByGPA.java
@@ -0,0 +1,11 @@
+package net.braniumacademy.lesson28.exercises1;
+
+
+import java.util.Comparator;
+
+public class SortingByGPA implements Comparator<Student> {
+    @Override
+    public int compare(Student o1, Student o2) {
+        return Float.compare(o1.getGpa(), o2.getGpa());
+    }
+}
