7851307d02862c4cdfc9f89f8a83fec0ac63d388
Trieu Than <trieuthan92pro@gmail.com>
2021-07-17 00:51:23 +0700

    Add ex1-2 l3.4

---
+++ src/net/braniumacademy/lesson34/exercises2/Exercises2.java
@@ -0,0 +1,37 @@
+package net.braniumacademy.lesson34.exercises2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * @author Branium Academy
+ * @version 2021.07
+ * @website https://braniumacademy.net/
+ */
+
+public class Exercises2 {
+    public static void main(String[] args) {
+        Stack<String> words = new Stack<>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhập vào một câu bạn muốn nói: ");
+        var str = input.nextLine();
+        var elements = str.split("\\s+"); // tách từ
+        System.out.println(Arrays.deepToString(elements));
+        reverseWords(words, elements); // đảo từ
+        showResult(words); // hiện kết quả
+    }
+
+    private static void showResult(Stack<String> words) {
+        while (!words.isEmpty()) {
+            System.out.print(words.peek() + " ");
+            words.pop();
+        }
+    }
+
+    private static void reverseWords(Stack<String> words, String[] elements) {
+        for (var e : elements) {
+            words.push(e);
+        }
+    }
+}
