2027ff7cde891cc25435eb3d8e172fbd97f9081b
Trieu Than <trieuthan92pro@gmail.com>
2021-07-18 18:40:22 +0700

    Add exercises10

--- src/net/braniumacademy/lesson15/Exercises10.java
+++ src/net/braniumacademy/lesson15/Exercises10.java
@@ -1,7 +1,32 @@
 package net.braniumacademy.lesson15;
 
-public class Exercises10 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Exercises10 {
+    private static long r[] = new long[1000000]; // tối đa 100k phần tử
+
+    private static long coin(long n) {
+        if (n == 0) { // trường hợp cơ sở
+            return 0;
+        } else if (n < 12) { // trường hợp cơ sở
+            return n;
+        } else {
+//            if (n < 1000000) {
+                if (r[(int) n] > 0) return r[(int) n];
+//            }
+            long m = Math.max(n, coin(n / 2) + coin(n / 3) + coin(n / 4));
+            if (n < 1000000) {
+                r[(int) n] = m;
+            }
+            return m;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLong()) { // đọc các bộ test chừng nào chưa hết các bộ test
+            long n = sc.nextLong();
+            System.out.println(coin(n));
+        }
     }
 }
