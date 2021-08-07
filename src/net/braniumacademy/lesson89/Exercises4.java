c0e744fad7c2acd13fe56079cc566381808dd3f2
Trieu Than <trieuthan92pro@gmail.com>
2021-08-08 00:24:23 +0700

    Add ex l89

---
+++ src/net/braniumacademy/lesson89/Exercises4.java
@@ -0,0 +1,80 @@
+package net.braniumacademy.lesson89;
+
+import java.util.Scanner;
+
+/**
+ * @author Branium Academy
+ * @version 2021.08
+ * @website https://braniumacademy.net/
+ */
+
+public class Exercises4 {
+    // thuật toán couting sort
+    public static void countingSort(int[] input, int exp) {
+        int n = input.length;
+        int k = 9;
+        int[] count = new int[k + 1];
+        int[] output = new int[n];
+        for (int i = 0; i < n; i++) {
+            int j = (input[i] / exp) % 10;
+            count[j]++;
+        }
+        // sắp xếp giảm dần
+        for (int i = k - 1; i >= 0; i--) {
+            count[i] += count[i + 1];
+        }
+        // đưa các phần tử vào đúng vị trí của nó
+        for (int i = n - 1; i >= 0; i--) {
+            int j = (input[i] / exp) % 10;
+            count[j]--;
+            output[count[j]] = input[i];
+        }
+        // sao chép các phần tử trong mảng output vào mảng gốc input
+        for (int i = 0; i < n; i++) {
+            input[i] = output[i];
+        }
+    }
+
+    // thuật toán radix sort
+    public static void radixSort(int[] arr) {
+        int max = findMax(arr);
+        for (int i = 1; max / i > 0; i *= 10) {
+            countingSort(arr, i);
+            showElements(arr);
+        }
+    }
+
+    // thuật toán tìm max trong mảng
+    private static int findMax(int[] arr) {
+        int max = arr[0];
+        for (var e : arr) {
+            if (e > max) {
+                max = e;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        var input = new Scanner(System.in);
+        var t = input.nextInt();
+        int test = 1;
+        while (t > 0) {
+            int n = input.nextInt();
+            var arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = input.nextInt();
+            }
+            System.out.printf("Test %d:\n", test++);
+            radixSort(arr);
+            t--;
+        }
+    }
+
+    private static void showElements(int[] arr) {
+        for (var e : arr) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+    }
+}
