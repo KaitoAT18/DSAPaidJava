7577f6d64c221db461c4558edd31f57b19d27688
Trieu Than <trieuthan92pro@gmail.com>
2021-08-11 15:44:10 +0700

    Add update exercises3-4 l1.9

--- src/net/braniumacademy/lesson19/Exercises3.java
+++ src/net/braniumacademy/lesson19/Exercises3.java
@@ -1,5 +1,7 @@
 package net.braniumacademy.lesson19;
 
+import java.util.Scanner;
+
 /**
  * @author Branium Academy
  * @version 2021.08
@@ -7,4 +9,69 @@ package net.braniumacademy.lesson19;
  */
 
 public class Exercises3 {
+    // sinh hoán vị kế tiếp
+    public static boolean nextPermutation(int[] arr) {
+        int i = arr.length - 2;
+        while (i >= 0 && arr[i] > arr[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int k = arr.length - 1;
+            while (arr[i] > arr[k]) {
+                k--;
+            }
+            int tmp = arr[i];
+            arr[i] = arr[k];
+            arr[k] = tmp;
+            int r = i + 1;
+            int s = arr.length - 1;
+            while (r < s) {
+                int t = arr[r];
+                arr[r] = arr[s];
+                arr[s] = t;
+                r++;
+                s--;
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // thuật toán sinh hoán vị chính tắc
+    public static void generatePermutation(int[] arr) {
+        boolean isFinalConfig = nextPermutation(arr);
+        if (isFinalConfig) {
+            System.out.println("UNAVAILABLE");
+        } else {
+            while (!isFinalConfig) {
+                output(arr);
+                isFinalConfig = nextPermutation(arr);
+            }
+        }
+    }
+
+    private static void output(int[] arr) {
+        for (var e : arr) {
+            System.out.printf("%d ", e);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        var input = new Scanner(System.in);
+        var t = input.nextInt();
+        input.nextLine();
+        var count = 1;
+        while (t-- > 0) {
+            var n = input.nextInt(); // đọc n
+            var arr = new int[n]; // tạo mảng với các giá trị mặc định
+            for (int i = 0; i < n; i++) { // đọc các phần tử của cấu hình cho trước
+                arr[i] = input.nextInt();
+            }
+            System.out.printf("Test %d: \n", count++);
+            generatePermutation(arr);
+        }
+        input.close();
+    }
 }
--- src/net/braniumacademy/lesson19/Exercises4.java
+++ src/net/braniumacademy/lesson19/Exercises4.java
@@ -1,5 +1,7 @@
 package net.braniumacademy.lesson19;
 
+import java.util.Scanner;
+
 /**
  * @author Branium Academy
  * @version 2021.08
@@ -7,4 +9,67 @@ package net.braniumacademy.lesson19;
  */
 
 public class Exercises4 {
+    // sinh hoán vị kế tiếp
+    public static boolean nextPermutation(int[] arr) {
+        int i = arr.length - 2;
+        while (i >= 0 && arr[i] > arr[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int k = arr.length - 1;
+            while (arr[i] > arr[k]) {
+                k--;
+            }
+            int tmp = arr[i];
+            arr[i] = arr[k];
+            arr[k] = tmp;
+            int r = i + 1;
+            int s = arr.length - 1;
+            while (r < s) {
+                int t = arr[r];
+                arr[r] = arr[s];
+                arr[s] = t;
+                r++;
+                s--;
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // thuật toán sinh hoán vị chính tắc
+    public static void generatePermutation(String[] names) {
+        var arr = new int[names.length];
+        // khởi tạo
+        for (int i = 0; i < names.length; i++) {
+            arr[i] = i + 1;
+        }
+        var isFinalConfig = false;
+        while (!isFinalConfig) {
+            output(names, arr);
+            isFinalConfig = nextPermutation(arr);
+        }
+    }
+
+    private static void output(String[] names, int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%s ", names[arr[i] - 1]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        var input = new Scanner(System.in);
+        var t = input.nextInt();
+        input.nextLine();
+        var count = 1;
+        while (t-- > 0) {
+            var test = input.nextLine(); // đọc n
+            var names = test.split("\\s+"); // tạo mảng với các giá trị mặc định
+            System.out.printf("Test %d: \n", count++);
+            generatePermutation(names);
+        }
+        input.close();
+    }
 }
